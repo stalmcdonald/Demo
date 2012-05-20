32198e1490baf7fa4ba3cacbc2d20192daf11ecf
Crystal McDonald <stalmcdonald@fullsail.edu>
2012-05-19 20:07:09 -0700

    Trying to fix camera feature.  all other features working.

--- www/js/camera.js
+++ www/js/camera.js
@@ -1,37 +1,71 @@
-/*
-function take_pic(){
-  var viewport = document.getElementById('viewport');
-  viewport.style.display = "";
-  navigator.camera.getPicture(dump_pic, fail, { quality: 50 }); 
+//Credit to Hutley-HelloPhoneGap https:github.com/hutley/HelloPhoneGap
+
+var pictureSource; // picture source
+var destinationType; // sets the format of returned value
+
+
+function onBodyLoad()
+{
+    document.addEventListener("deviceready",onDeviceReady,false);
 }
 
-function dump_pic(data){
-  var viewport = document.getElementById('viewport');
-  console.log(data);
-  viewport.style.display = "block";
-  viewport.style.position = "absolute";
-  viewport.style.top = "10px";
-  viewport.style.left = "10px";
-  document.getElementById("test_img").src = "data:image/jpeg;base64," + data;
+/* When this function is called, PhoneGap has been initialized and is ready to roll */
+function onDeviceReady()
+{
+    phoneGapReady.innerHTML = "";
+    pictureSource=navigator.camera.PictureSourceType;
+    destinationType=navigator.camera.DestinationType;
+    
 }
-*/
-function PictureSourceType() {};
-PictureSourceType.PHOTO_LIBRARY = 0;
-PictureSourceType.CAMERA = 1;
- 
-function getPicture(sourceType)
-{
-     var options = { quality: 10 };
-     if (sourceType != undefined) {
-          options["sourceType"] = sourceType;
-     }
-     // if no sourceType specified, the default is CAMERA 
-     navigator.camera.getPicture(getPicture_Success, null, options);
-};
 
- 
-function getPicture_Success(imageData)
-{
-         alert("getpic success");
-        document.getElementById("test_img").src = "data:image/jpeg;base64," + imageData;
+// Called when a photo is successfully retrieved
+function onPhotoDataSuccess(imageData) {
+
+    // Get image handle
+    var myImage = document.getElementById('myImage');
+    
+    // Unhide image elements
+    myImage.style.display = 'block';
+    
+    // Show the captured photo
+    myImage.src = "data:image/jpeg;base64," + imageData;
+    
+}
+
+// Called when a photo is successfully retrieved
+function onPhotoURISuccess(imageURI) {
+    
+    // Get image handle
+    var lgImage = document.getElementById('lgImage');
+    
+    // Unhide image elements
+    lgImage.style.display = 'block';
+    
+    // Show the captured photo
+    lgImage.src = imageURI;
+}
+
+// A button will call this function
+function capturePhoto() {
+    
+    // Take picture using device camera and retrieve image as base64-encoded string
+     {
+        navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 90
+                                     });
+      }
+        
+
+// A button will call this function
+function getPhoto(source) {
+    
+     {
+        // Retrieve image file location from specified source
+        navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,
+                                    destinationType: destinationType.FILE_URI,
+                                    sourceType: source });
+}
+
+function onFail(message) {
+    
+    alert('Failed: ' + message);
 }
